5c2120481dc43ce4cc7bc51e44060c91ecda7a21
Asad1ama <asad.ama339@gmail.com>
2026-07-07 13:32:39 +0530

    Day 01: Python Basics

---
+++ 01-Python/Day-1.py
@@ -0,0 +1,131 @@
+#python : interpreted languages During runtime, line-by-line from source code
+
+# comments ,"""" ___ """ doc stringfor multi line comments
+
+#Asad
+""" Asad 
+Asad
+Asad """
+
+AsadAmanullah="coder" #pascal case
+asadAmanullah="coding" #camel case
+asad_amanullah ="coding" #snake case
+
+
+""" Datatypes  """
+# a=12
+# print(type(a))
+
+# b=12/3
+# print(type(b))
+
+# v=34j
+# print (type(v))
+
+# string_one = 'Hello World'
+# print(type(string_one))
+
+# string_two = "Hello World"
+# print(type(string_two)) 
+
+
+""" strings """
+
+# a='A'
+# print(ord(a))  #converts character to unicode
+
+# a=65
+# print(chr(a))  #converts unicode to character
+
+# a= "Asad Amanullah"  # index from front 0,1,2,3 ,from back -1,-2,-3,-4
+# print (a[1],a[-1])
+
+ 
+#  #slicing in python: sequence[start:stop:step]
+# print(a[0:4:2])
+ 
+
+### typeconversion
+
+## #explicit
+# a="12"
+# a=int(12)
+# print(type(a))
+
+##implicit
+# b=12/3
+# print(type(b))
+
+##### input function
+# print('Enter your name:')
+# x = input()
+# print('Hello, ' + x)
+# print("Hello,",x)
+# print(f"Hello, {x}") #formatted string
+
+####operators
+
+##arithmetic
+# a=10 
+# b=20
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(b/a) ##gives float type
+# print(b//a) ##gives int only
+# print(5**3)
+# print(5**2)
+# print(b%a)
+
+
+## assignment operators
+# a=23
+
+## compound assignment operators :we can reassign on same variable and it will give the output of the latest value or operations done
+# a=20
+# a+=20
+# a+=30
+# print(a)
+
+##comparison operators 
+# a=10
+# b=20
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print("ABC">"ACD") # FALSE BCZ LETTER BY LETTER CHECK ASCII
+# print(a<b)
+# print(a&b)
+# print(a|b)
+
+### logical
+# print((a==b) and (a!=b))
+# print((a==b) or (a!=b))
+# print( not (a!=b))
+# print(True and bool(0))
+
+
+
+### conditional statement: if, if-else .if-elif-else
+### if else 
+
+# a=13
+# if(a>13):
+#     print("hii")
+# else:
+#     print("bye")
+
+###if elif else
+
+# x=int(input("Enter no:"))
+# if(x>20):
+#     print("Yes")
+# elif (x==20):
+#     print("Bye")
+# else:
+#     print("sojao")
+
+
+
+
+
